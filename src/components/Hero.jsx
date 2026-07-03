362547e0766ab403cefe3551971ddf2c5411f472
arpann-singh <arpans7@outlook.in>
2026-07-03 21:16:45 +0530

    resolve try

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -105,7 +105,7 @@ const Hero = () => {
             aria-label="Register via Devfolio"
           >
             <img
-              src="/assets/devfolio-btn.png"
+              src="/assets/devfolio-logo.png"
               alt="DEVFOLIO LOGO"
               className="h-14 w-auto object-contain cursor-pointer"
             />
